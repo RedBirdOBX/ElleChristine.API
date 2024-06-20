b9e39e406c598bfe5ce3c2e5875112eb7b5428fe
RedBirdOBX <shane.fowlkes.70@gmail.com>
2024-06-20 16:14:21 -0400

    added photos script

---
+++ docs/sql scripts/create-photos-tbl.sql
@@ -0,0 +1,18 @@
+-- Table: public.photos
+
+-- DROP TABLE IF EXISTS public.photos;
+
+CREATE TABLE IF NOT EXISTS public.photos
+(
+    id integer NOT NULL DEFAULT nextval('photos_id_seq'::regclass),
+    file_name character varying COLLATE pg_catalog."default" NOT NULL,
+    heading character varying COLLATE pg_catalog."default" NOT NULL,
+    description character varying COLLATE pg_catalog."default" NOT NULL,
+    active boolean NOT NULL DEFAULT true,
+    CONSTRAINT photos_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.photos
+    OWNER to postgres;
